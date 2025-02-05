4ad15ec4d94f62dcadffc7f47fbbdbc53f299018
BugHunter-X <askzenai@gmail.com>
2025-02-05 19:04:07 +0100

    Add BugComponent.js file

---
+++ BugComponent.js
@@ -0,0 +1,1 @@
+This error occurs when you try to use a component before it has been defined. This typically happens when you import a component that uses another component that hasn't been imported in that file, creating a circular dependency, or simply importing a component before it has been defined.
